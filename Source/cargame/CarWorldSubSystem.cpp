30c61923b267c8e7bf5ebe09f5a01805396e14f6
dbustan <danielbustan100@gmail.com>
2025-12-26 17:43:07 -0800

    Now spawns more road! but transform is bugged.

--- Source/cargame/CarWorldSubSystem.cpp
+++ Source/cargame/CarWorldSubSystem.cpp
@@ -9,20 +9,37 @@ void UCarWorldSubSystem::Initialize(FSubsystemCollectionBase& Collection)
 	UWorld* World = GetWorld();
 	
 	if (World) {
-		//you need to pass a function address specifically
+		//you need to pass a function address specifically to link OnWorldBeginPlay to Trigger OnLevelStart
 		World->OnWorldBeginPlay.AddUObject(this, &UCarWorldSubSystem::OnLevelStart);
 	}
-
+	//Since you can't make a blueprint class of a subsystem we have to gather the road from our folder
+	FString RoadPath = TEXT("/Game/BPs/BP_Road.BP_Road_C");
+	RoadToSpawn = StaticLoadClass(AActor::StaticClass(), nullptr ,*RoadPath);
+	if (!RoadToSpawn) {
+		UE_LOG(LogTemp, Error, TEXT("Road failed to load"));
+	}	
 }
 
 void UCarWorldSubSystem::Tick(float DeltaTime)
 {
 	APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnInfo.Owner = nullptr;
 	FVector ActorLocation = PlayerPawn->GetActorLocation();
 	float SpawnDistance = (ActorLocation - Spawn.GetLocation()).Size();
-
-	if (SpawnDistance < 300000) {
-		/*GetWorld()->SpawnActor<ARoad>(Spawn, );*/
+	/*AActor* SpawnedActor = GetWorld()->SpawnActor<AInteractables>(PhoneToSpawn, FVector::ZeroVector, PhonePos->GetComponentRotation(), SpawnParams);*/
+	UE_LOG(LogTemp, Warning, TEXT("%f"), SpawnDistance);
+	if (SpawnDistance < 10000 && !RoadSpawned) {
+		RoadSpawned = true;
+		ARoad* Road = GetWorld()->SpawnActor<ARoad>(RoadToSpawn, Spawn, SpawnInfo);
+		if (Road == NULL) {
+			UE_LOG(LogTemp, Warning, TEXT("dada"));
+		}
+		else {
+			Spawn = Road->GetArrowComponent()->GetComponentTransform();
+			
+		}
 	}
 
 }
@@ -34,5 +51,5 @@ void UCarWorldSubSystem::OnLevelStart()
 	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("Road"), FoundActors);
 	ARoad* Road = Cast<ARoad>(FoundActors[0]);
 	Spawn = Road->GetArrowComponent()->GetComponentTransform();
-	
+	RoadSpawned = false;
 }
--- Source/cargame/CarWorldSubSystem.h
+++ Source/cargame/CarWorldSubSystem.h
@@ -7,7 +7,6 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "Tickable.h"
 #include  "Kismet/GameplayStatics.h"
-#include "FVector.h"
 #include "Components/ArrowComponent.h"
 #include "Subsystems/SubsystemCollection.h"
 #include "CarWorldSubSystem.generated.h"
@@ -21,10 +20,20 @@ class CARGAME_API UCarWorldSubSystem : public UWorldSubsystem, public FTickableG
 	GENERATED_BODY()
 protected:
 	FTransform Spawn;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class ARoad> RoadToSpawn;
+	bool RoadSpawned;
 public:
 
 protected:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	//Everything here has to be inherited by a class to use FTickableGameObject
+	//Code will not work unless you do
 	virtual void Tick(float DeltaTime) override;
+	virtual bool IsTickable() const override { return !IsTemplate(); }
+	virtual TStatId GetStatId() const override { RETURN_QUICK_DECLARE_CYCLE_STAT(UCarWorldSubSystem, STATGROUP_Tickables); }
+	virtual UWorld* GetTickableGameObjectWorld() const override { return GetWorld(); }
 	void OnLevelStart();
 };
